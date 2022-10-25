903cf13b3fd1aeffc0477abafd3b00b9f540636a
Alan Bangura <abangura18@gmail.com>
2022-10-24 21:28:37 -0400

    RPS Revisited

--- javascript.js
+++ javascript.js
@@ -1,51 +1,130 @@
-game();
+// Create HTML for game
+const rpsContainer = document.createElement('div');
+rpsContainer.setAttribute('id','rps-container');
+rpsContainer.style.cssText = `
+    display: flex;
+    flex-direction: column;
+    // justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+`;
 
-// true = Player wins; false = CPU wins
-function rpsWinner(playerDecision,cpuDecision) {
-    let isWinner;
-    // Checks all the different permutations of outcomes.
-    if(playerDecision == 'paper' && cpuDecision == 'rock') isWinner = true;
-    else if(playerDecision == 'paper' && cpuDecision == 'scissors') isWinner = false;
-    else if(playerDecision == 'rock' && cpuDecision == 'scissors') isWinner = true;
-    else if(playerDecision == 'rock' && cpuDecision == 'paper') isWinner = false;
-    else if(playerDecision == 'scissors' && cpuDecision == 'paper') isWinner = true;
-    else if(playerDecision == 'scissors' && cpuDecision == 'rock') isWinner = false;
+const title = document.createElement('div');
+title.setAttribute('id','title');
+title.textContent = 'Player vs. CPU';
+title.style.cssText = `
+    text-align: center;
+`
 
-    return isWinner;
-}
+const headerContainer = document.createElement('div');
+headerContainer.setAttribute('id','header-container');
+headerContainer.style.cssText = `
+    padding: 10px;
+    border: solid black 1px;
+    background-color: lightgrey;
+`;
 
-function game() {
-    let playerScore = 0;
-    let cpuScore = 0;
+const buttonContainer = document.createElement('div');
+buttonContainer.setAttribute('id','button-container');
+buttonContainer.style.cssText = `
+    margin: 20px;
+`;
+
+const rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+const paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'Scissors';
+
+buttonContainer.appendChild(rockButton);
+buttonContainer.appendChild(paperButton);
+buttonContainer.appendChild(scissorsButton);
+
+headerContainer.appendChild(title);
+headerContainer.appendChild(buttonContainer);
+rpsContainer.appendChild(headerContainer);
+
+const scoreBoard = document.createElement('div');
+const player = document.createElement('div');
+const cpu = document.createElement('div');
+const winner = document.createElement('div');
+
+// Keep track of score
+let round = 1;
+let playerScore = 0;
+let cpuScore = 0;
+player.textContent = 'Player 1: ' + playerScore;
+cpu.textContent = 'CPU: ' + cpuScore;
+
+scoreBoard.setAttribute('id','score-board');
+scoreBoard.style.cssText = `
+    margin:10px;
+`
+scoreBoard.appendChild(player);
+scoreBoard.appendChild(cpu);
+scoreBoard.appendChild(winner);
+rpsContainer.appendChild(scoreBoard);
+
+document.body.appendChild(rpsContainer)
+
+
+// Let players click rps button to decide move.
+console.log('Let\s play rock paper scissors');
+console.log('First opponent to score 3 points wins!');
+
+rockButton.addEventListener('click', e => {playGame(e)});
+paperButton.addEventListener('click', e => {playGame(e)});
+scissorsButton.addEventListener('click', e => {playGame(e)});
+
+
+// Event action listener to play game by picking rock,paper, or scissor button.
+function playGame(e) {
     let isWinner;
     let playerDecision;
     let cpuDecision;
 
     let rpsArray = ['rock','paper','scissors'];
 
-    for(let i = 0; i < 5; i++) {
-        // Let user enter rock, paper, or scissors
-        playerDecision = prompt("rock, paper, scissors");
-        playerDecision = playerDecision.toLowerCase();
-        cpuDecision = rpsArray[Math.floor(Math.random()*3)];
+    if(playerScore == 3) {return;} 
+    if(cpuScore == 3) {return;}
 
-        while(!rpsArray.includes(playerDecision) || playerDecision == cpuDecision) {
-            playerDecision = prompt("rock, paper, scissors");
-            playerDecision = playerDecision.toLowerCase();
-            cpuDecision = rpsArray[Math.floor(Math.random()*3)];
-        }
-
-        isWinner = rpsWinner(playerDecision,cpuDecision);
-
-        if(isWinner) {
-            playerScore++;
-            console.log(`Round ${i+1}\nPlayer: ${playerDecision}\nCPU: ${cpuDecision}\nWinner: Player`);
-        }
-        else {
-            cpuScore++;
-            console.log(`Round ${i+1}\nPlayer: ${playerDecision}\nCPU: ${cpuDecision}\nWinner: CPU`);
-        }
+    playerDecision = e.target.innerText.toLowerCase();
+    cpuDecision = rpsArray[Math.floor(Math.random()*3)];
+    
+    isWinner = rpsWinner(playerDecision,cpuDecision);
+    
+    if(isWinner == 1) {
+        playerScore++;
+        player.textContent = 'Player: ' + playerScore;
+        console.log(`Round ${round}\nPlayer: ${playerDecision}\nCPU: ${cpuDecision}\nWinner: Player`);
+    }
+    else if(isWinner == 2) {
+        cpuScore++;
+        cpu.textContent = 'CPU: ' + cpuScore;
+        console.log(`Round ${round}\nPlayer: ${playerDecision}\nCPU: ${cpuDecision}\nWinner: CPU`);
+    }
+    else {
+        console.log(`Round ${round}\nPlayer: ${playerDecision}\nCPU: ${cpuDecision}\nTie`);
     }
 
-    console.log(`Player Score: ${playerScore}\nCPU Score: ${cpuScore}`);
+    round++;
+
+    if(playerScore == 3) {winner.textContent = 'Player Wins!'; return;} 
+    if(cpuScore == 3) {winner.textContent = 'CPU Wins!'; return;}
+}
+
+// 0 = Tie Game; 1 = Player wins; 2 = CPU wins
+function rpsWinner(playerDecision,cpuDecision) {
+    let isWinner;
+    // Checks all the different permutations of outcomes.
+    if(playerDecision == 'paper' && cpuDecision == 'rock') isWinner = 1;
+    else if(playerDecision == 'paper' && cpuDecision == 'scissors') isWinner = 2;
+    else if(playerDecision == 'rock' && cpuDecision == 'scissors') isWinner = 1;
+    else if(playerDecision == 'rock' && cpuDecision == 'paper') isWinner = 2;
+    else if(playerDecision == 'scissors' && cpuDecision == 'paper') isWinner = 1;
+    else if(playerDecision == 'scissors' && cpuDecision == 'rock') isWinner = 2;
+    else isWinner = 0;
+
+    return isWinner;
 }
